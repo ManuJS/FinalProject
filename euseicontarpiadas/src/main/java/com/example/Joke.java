7b45f01433a1ade649f8f75bb87fceae10af33d9
ManuJS <emanuelle.menali@hotmail.com>
2017-02-02 02:01:15 -0200

    Initial commit

---
+++ euseicontarpiadas/src/main/java/com/example/Joke.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Joke {
+
+    public void Joke(){
+
+        String piada1 = "piada 1";
+        String piada2 = "piada 2";
+        String piada3 = "piada 3";
+        String piada4 = "piada 4";
+        String piada5 = "piada 5";
+        String piada6 = "piada 6";
+        String piada7 = "piada 7";
+    }
+}
